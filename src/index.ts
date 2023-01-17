f82184334cdb180a62dbd6ce9117dd20f1b74446
Masha0701 <reflection2008@gmail.com>
2023-01-17 23:52:40 +0300

    init: webpack configured

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './styles.scss';
+import './assets/img_5-1.png';
+
+document.body.innerHTML = '<img src="./assets/img_5-1.png" />'
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+      open: true,
+      port: 8080,
+      //contentBase: path.join(__dirname, 'public'),
+    },
+};
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  devtool: develop ? 'inline-source-map' : false,
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules:[
+        {
+            test:/\.[tj]s$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+          type: 'asset/resource',
+        },
+        {
+            test:/\.(woff(2)?|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    ...esLintPlugin(develop),
+    //new CopyPlugin({
+    //    patterns: [
+    //        {
+    //            from: './public',
+    //        }
+    //    ]
+    //}),
+    
+  ],
+  ...devServer(develop),
+});
