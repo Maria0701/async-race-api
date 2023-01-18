91ea32e5141f747fe81a00b2c6af862dbd6b80d2
Masha0701 <reflection2008@gmail.com>
2023-01-18 20:21:34 +0300

    feat: custom ds for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import './styles.scss';
-import './assets/img_5-1.png';
+import img  = require('./assets/img_5-1.png');
 
-document.body.innerHTML = '<img src="./assets/img_5-1.png" />'
+document.body.innerHTML = `<img src="${img}" />`;
